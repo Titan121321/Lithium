993e03793331a3d58bcd66bf46d7a22302504810
HaRi-826 <harikrishnanv2006@gmail.com>
2025-07-27 06:08:02 +0000

    Update score script.js

--- Questgame/Questgame/score script.js
+++ Questgame/Questgame/score script.js
@@ -3,7 +3,7 @@ function goToVanquished() {
 }
 
 window.onload = () => {
-  const username = localStorage.getItem('username') || 'Unknown';
+  const username = localStorage.getItem('username') || 'User123';
   const score = localStorage.getItem('userScore') || '0';  // ✅ fixed key to match vanquest.js
   document.getElementById('scoreDisplay').innerText = `${username}: ${score} pts`;
 };
